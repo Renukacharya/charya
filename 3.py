0b6ceb24d67fe92110eb2a5d2cf680185bd2a2f7
Renukacharya <31615147+Renukacharya@users.noreply.github.com>
2017-09-07 09:24:34 +0530

    Add files via upload

--- 3.py
+++ 3.py
@@ -9,30 +9,38 @@ while count<=100:
 		count=count+r
 		print("your random num is",r)
 		print("you are on count",count)
-	elif count=="8":
-		  count=37
+	if count==8:
+		count=37
 		print("you climb the ladder",count)
-	if count=="13":
+	if count==13:
 		count=34
 		print("you climb the ladder",count)
-	elif count=="25":
-		  count=4
-		print("snake ate you down",count)
-	if count=="11":
-		count=2
-		print("snake ate you down",count)
-	elif count=="25":
+	if count==25:
 		count=4
 		print("snake ate you down",count)
-	if count=="38":
+	if count==11:
+		count=2
+		print("snake ate you down",count)
+	if count==38:
 		count=9
 		print("snakea ate you down",count)
-	elif count=="40":
+	if count==40:
 		count=68
 		print("you climb the ladder",count)
-	if count=="65":
+	if count==52:
+		count=81
+		print("you climb the ladder",count)  
+	if count==65:
 		count=46
 		print("snake ate you down",count)
-	elif count==""	 
-				
-				
+	if count==68:
+		count=40
+		print("snake ate you down",count)
+	if count==76:
+		count=97
+		print("you climb the ladder",count)	  
+	if count==89:
+		count=70
+		print("snake ate you down",count)  
+	if count>=100
+		print("congragulations")
---
+++ 5.py
@@ -0,0 +1,5 @@
+#! /usr/bin/python3
+num = int(input("enter the number: "))
+for i in range (1,11):
+   for j in range (1,11):
+	   print (i,"x",j,"=",i*j)		
